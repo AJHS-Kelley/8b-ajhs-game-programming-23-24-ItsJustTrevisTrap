a4d3fec5bbdc5e3b4641d19818fdb320af18e72f
ItsJustTrevisTrap <142345729+ItsJustTrevisTrap@users.noreply.github.com>
2023-12-12 14:27:58 -0500

    Update oop.py

--- gameProgramming/05a_exampleGameFunctions/oop.py
+++ gameProgramming/05a_exampleGameFunctions/oop.py
@@ -1,3 +1,10 @@
 # Object-Oriented Programming, Trevis Brown, v0.0  
 
 class Person: # Use PascalCase for ClassNames
+    def  __init__(self, name, age, weight):
+        self.name = name
+        self.age = age
+        self.weight = weight
+
+
+person1 = Person()
