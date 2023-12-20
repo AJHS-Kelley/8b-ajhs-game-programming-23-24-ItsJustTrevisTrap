e84d29d3d87a133adebffd571678470289abe294
ItsJustTrevisTrap <142345729+ItsJustTrevisTrap@users.noreply.github.com>
2023-12-20 14:19:43 -0500

    Update exampleGameFunctions.py

--- gameProgramming/05a_exampleGameFunctions/exampleGameFunctions.py
+++ gameProgramming/05a_exampleGameFunctions/exampleGameFunctions.py
@@ -1,15 +1,15 @@
 # Example Game Functions Project, Trevis Brown, v0.0
-import pygame
+#import pygame
 import random
 
 # Initialize pygame
-pygame.init()
+#pygame.init()
 
 # Set up the game window
 width = 800
 height = 600
-window = pygame.display.set_mode((width, height))
-pygame.display.set_caption("Racing Game")
+#window = pygame.display.set_mode((width, height))
+#pygame.display.set_caption("Racing Game")
 
 # Set up the colors
 white = (255, 255, 255)
@@ -19,8 +19,31 @@ red = (255, 0, 0)
 # Set up the player car
 car_width = 73
 car_height = 82
-car_img = pygame.image.load("car.png")
-car_img = pygame.transform.scale(car_img, (car_width, car_height))
+#car_img = pygame.image.load("car.png")
+#car_img = pygame.transform.scale(car_img, (car_width, car_height))
 car_x = width // 2 - car_width // 2
 car_y = height - car_height - 10
 car_speed = 5     
+
+# Set up the enemy car
+enemy_width = 73
+enemy_height = 82
+#car_img = pygame.image.load("car.png")
+#car_img = pygame.transform.scale(car_img, (car_width, car_height))
+car_x = width // 2 - car_width // 2
+car_y = height - car_height - 10
+car_speed = 5
+
+# Set up the team car 
+team_width = 73
+team_height = 82
+#car_img = pygame.image.load("car.png")
+#car_img = pygame.transform.scale(car_img, (car_width, car_height))
+car_x = width // 2 - car_width // 2
+car_y = height - car_height - 10
+car_speed = 5
+
+
+
+
+
