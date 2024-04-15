035f5d7aca6656388874062b075ca21cd29c5efd
ItsJustTrevisTrap <142345729+ItsJustTrevisTrap@users.noreply.github.com>
2024-04-15 13:42:51 -0400

    Update finalproject.py

--- 09_finalProject/finalproject.py
+++ 09_finalProject/finalproject.py
@@ -1,23 +1,5 @@
 # Final Project, Trevis brown, v0.0
-import sys, random, pygame
-
-resolution = 0 # 0 = Low Resolution, 1 = High Resolution
-
-if resolution == 0:
-    x = 800
-    y = 600
-else
-    x = 1920
-    y = 1080
-
-pygame.init()
-
-difficulty = int(input("Please choose a difficulty. Enter 1 for EASY or 2 for HARD.\n"))
-
-pygame.display.set_caption('NAME OF GAME -- EASY')
-pygame.display.set_caption('NAME OF GAME -- HARD')
-
-screen = pygame.display.set_mode((x, y))
+import sys
  
 
 
