d5c534082f3d9473bb40c29d16707d57127d808c
ItsJustTrevisTrap <142345729+ItsJustTrevisTrap@users.noreply.github.com>
2024-04-15 14:44:03 -0400

    Update finalproject.py

--- 09_finalProject/finalproject.py
+++ 09_finalProject/finalproject.py
@@ -2,10 +2,21 @@
 import pygame
 from sys import exit
 
+screen = pygame.display.set_mode((800,400))
+pygame.display.set_caption('Runner')
+clock = pygame.time.Clock()
 
+test_surface = pygame.Surface((100,200))
+test_surface.fill('Grey')
 
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            exit()        
 
+    screen.blit(test_surface,(0,0))
 
-
-
+    pygame.display.update()
+    clock.tick(60)
 
