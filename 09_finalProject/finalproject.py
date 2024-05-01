83e3551fa1457527e58a4d3bdc111bb479b019c1
ItsJustTrevisTrap <142345729+ItsJustTrevisTrap@users.noreply.github.com>
2024-05-01 13:44:05 -0400

    Update finalproject.py

--- 09_finalProject/finalproject.py
+++ 09_finalProject/finalproject.py
@@ -1,21 +1,28 @@
 import pygame
-from sys import exit
+import random
 
+#Constants
+WIDTH = 800
+HEIGHT = 600
+CELL_SIZE = 40
+ROWS = HEIGHT // CELL_SIZE
+COLS = WIDTH // CELL_SIZE
+
+# Colors
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+# Initialize Pygame
 pygame.init()
-screen = pygame.display.set_mode((800,400))
-pygame.display.set_caption('Runner')
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
 clock = pygame.time.Clock()
 
-mazeBackground = pygame.image.load('Final  project/maze.png')
+# Create maze
+maze = [[0 for _ in range(COLS)]]
 
-while True:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()
-            exit()        
+def generated_maze(x, y):
+    maze[y][x] = 1 
 
-    screen.blit(mazeBackground,(0,0))
 
-    pygame.display.update()
-    clock.tick(60)
+
+
 
